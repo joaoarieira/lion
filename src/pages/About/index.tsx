354a138226d6e475a2c42c92a5f4f9cb6f7799ef
João Azevedo <joao.azevedo@ufv.br>
2022-02-27 17:42:05 -0300

    about and notices pages

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,50 @@
+import { Box, Grid, Link, Typography } from '@mui/material';
+import { useCallback } from 'react';
+import CrudHeader from '../../components/CrudHeader';
+
 export function About(): JSX.Element {
-  return <h1>About</h1>;
+  return (
+    <Box>
+      <CrudHeader title="Sobre o Lion" />
+
+      <Grid container maxWidth="800px" rowSpacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            O objetivo do projeto Lion é organizar os horários das aulas das
+            monitorias oferecidas nas universidades, focando inicialmente na UFV
+            - <i>Campus</i> Rio Paranaíba.
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            Autor: João Vitor Azevedo
+            <br />
+            <Link href="mailto:joao.azevedo@ufv.br">joao.azevedo@ufv.br</Link>
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            Repositórios:
+            <br />
+            front-end:{' '}
+            <Link href="https://github.com/joaoarieira/lion" target="_blank">
+              https://github.com/joaoarieira/lion
+            </Link>
+            <br />
+            back-end:{' '}
+            <Link
+              href="https://github.com/joaoarieira/lion-api"
+              target="_blank"
+            >
+              https://github.com/joaoarieira/lion-api
+            </Link>
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default About;
--- src/pages/Notices/index.tsx
+++ src/pages/Notices/index.tsx
@@ -1,5 +1,21 @@
+import { Box, Typography, Link } from '@mui/material';
+import CrudHeader from '../../components/CrudHeader';
+
 export function Notices(): JSX.Element {
-  return <h1>Notices</h1>;
+  return (
+    <Box>
+      <CrudHeader title="Editais" />
+
+      <Typography variant="body1" marginBottom="0.75rem">
+        Tenha acesso aos editais das monitorias na página oficial da{' '}
+        <b>Diretoria de Ensino</b>:
+      </Typography>
+
+      <Link href="https://dre.crp.ufv.br/?page_id=161" target="_blank">
+        https://dre.crp.ufv.br/?page_id=161
+      </Link>
+    </Box>
+  );
 }
 
 export default Notices;
